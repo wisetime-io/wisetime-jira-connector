e0e3da168e9e5cb8e564cae8dc297810d5bba7ed
Vy-Shane Xie <shane@node.mu>
2018-12-03 13:02:29 +0800

    Add test for tag duration calculator

---
+++ src/test/java/io/wisetime/connector/jira/utils/TagDurationCalculatorTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.jira.utils;
+
+import org.junit.jupiter.api.Test;
+
+import io.wisetime.connector.jira.testutils.FakeEntities;
+import io.wisetime.generated.connect.TimeGroup;
+import io.wisetime.generated.connect.User;
+
+import static io.wisetime.connector.jira.utils.TagDurationCalculator.tagDurationSecs;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author shane.xie@practiceinsight.io
+ */
+class TagDurationCalculatorTest {
+
+  private final FakeEntities fakeEntities = new FakeEntities();
+
+  @Test
+  void tagDurationSecs_zero_experience_rating() {
+    final User user = fakeEntities.randomUser().experienceWeightingPercent(0);
+    final TimeGroup timeGroup = fakeEntities.randomTimeGroup().user(user);
+
+    assertThat(tagDurationSecs(timeGroup))
+        .isEqualTo(0)
+        .as("Zero experience rating should result in zero duration to each tag");
+  }
+
+  @Test
+  void tagDurationSecs_divide_between_tags() {
+    final User user = fakeEntities.randomUser().experienceWeightingPercent(10);
+    final TimeGroup timeGroup = fakeEntities.randomTimeGroup()
+        .user(user)
+        .totalDurationSecs(100)
+        .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.DIVIDE_BETWEEN_TAGS);
+
+    assertThat(tagDurationSecs(timeGroup))
+        .isEqualTo(10.0 / timeGroup.getTags().size())
+        .as("Calculated duration should take into account experience rating and" +
+            "split the total duration between the tags");
+  }
+
+  @Test
+  void tagDurationSecs_whole_duration_to_each_tag() {
+    final User user = fakeEntities.randomUser().experienceWeightingPercent(10);
+    final TimeGroup timeGroup = fakeEntities.randomTimeGroup()
+        .user(user)
+        .totalDurationSecs(100)
+        .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.WHOLE_DURATION_TO_EACH_TAG);
+
+    assertThat(tagDurationSecs(timeGroup))
+        .isEqualTo(10)
+        .as("Calculated duration should take into account experience rating and " +
+            "assign whole total duration to each tag");
+  }
+}
