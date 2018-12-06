7014121b20220f7736c63301adfa14c8b0176211
Vy-Shane Xie <shane@node.mu>
2018-12-06 21:20:23 +0800

    Remove log output to reduce test log noise. Not super important to log this as migration is only done in tests.

--- src/test/java/io/wisetime/connector/jira/testutils/FlywayJiraTestDbModule.java
+++ src/test/java/io/wisetime/connector/jira/testutils/FlywayJiraTestDbModule.java
@@ -10,8 +10,6 @@ import com.google.inject.Provider;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 
@@ -22,12 +20,10 @@ public class FlywayJiraTestDbModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    bind(Flyway.class)
-        .toProvider(FlywayJiraProvider.class);
+    bind(Flyway.class).toProvider(FlywayJiraProvider.class);
   }
 
   private static class FlywayJiraProvider implements Provider<Flyway> {
-    private static final Logger log = LoggerFactory.getLogger(FlywayJiraProvider.class);
 
     @Inject
     private Provider<DataSource> dataSourceProvider;
@@ -36,18 +32,11 @@ public class FlywayJiraTestDbModule extends AbstractModule {
     public Flyway get() {
       Flyway flyway = new Flyway();
       flyway.setDataSource(dataSourceProvider.get());
-
       flyway.setBaselineVersion(MigrationVersion.fromVersion("0"));
       flyway.setBaselineOnMigrate(true);
-
-      // jira db schema scripts location
       flyway.setLocations("jira_db_schema/");
-
-      log.info("Starting Flyway migration...");
       flyway.migrate();
       flyway.validate();
-      log.info("Flyway migration done.");
-
       return flyway;
     }
   }
