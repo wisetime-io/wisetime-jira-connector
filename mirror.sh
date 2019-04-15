143a329d1243bb51f8603a0a01af04cd8c1dd476
Tom Haines <thomas.haines@practiceinsight.io>
2019-04-15 08:48:24 +0800

    add indentation

--- mirror.sh
+++ mirror.sh
@@ -7,18 +7,18 @@ then
 else
     # Ensure we can talk to GitHub
     mkdir -p ~/.ssh && \
-      ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts && \
+    ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts && \
     # Prepare our GitHub private key
-      mkdir -p /tmp/.ssh && \
+    mkdir -p /tmp/.ssh && \
     echo "${GITHUB_SSH_KEY_B64}" | base64 -d > /tmp/.ssh/github.key
-      chmod 600 /tmp/.ssh/github.key && \
+    chmod 600 /tmp/.ssh/github.key && \
     # Push to GitHub mirror
-      git remote set-url origin git@github.com:wisetime-io/wisetime-jira-connector.git && \
-      GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push -u origin master && \
+    git remote set-url origin git@github.com:wisetime-io/wisetime-jira-connector.git && \
+    GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push -u origin master && \
     # Cleanup in case we're running this locally on a dev machine
-      git remote set-url origin ssh://git@stash.practiceinsight.io:7999/connect/wisetime-jira-connector.git && \
-      rm -rf /tmp/.ssh && \
-      echo "Push to GitHub mirror complete"
+    git remote set-url origin ssh://git@stash.practiceinsight.io:7999/connect/wisetime-jira-connector.git && \
+    rm -rf /tmp/.ssh && \
+    echo "Push to GitHub mirror complete"
 
     if [ "$?" -ne "0" ]; then
         exit 1
