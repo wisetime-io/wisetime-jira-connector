04dd5b5c09f70b1f669fdc75930c1a2262bf202c
Tom Haines <thomas.haines@practiceinsight.io>
2019-04-15 10:18:34 +0800

    simplify github push step

--- mirror.sh
+++ mirror.sh
@@ -13,10 +13,9 @@ else
     echo "${GITHUB_SSH_KEY_B64}" | base64 -d > /tmp/.ssh/github.key
     chmod 600 /tmp/.ssh/github.key && \
     # Push to GitHub mirror
-    git remote set-url origin git@github.com:wisetime-io/wisetime-jira-connector.git && \
-    GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push -u origin master && \
-    # Cleanup in case we're running this locally on a dev machine
-    git remote set-url origin ssh://git@stash.practiceinsight.io:7999/connect/wisetime-jira-connector.git && \
+    git remote add github git@github.com:wisetime-io/wisetime-jira-connector.git && \
+    GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github master && \
+    GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github --tags && \
     rm -rf /tmp/.ssh && \
     echo "Push to GitHub mirror complete"
 
