e8ee1435d6be5b32ca7c0c913ae68954c7960e97
Jordan Duabe <jordan.duabe@staff.wisetime.com>
2019-05-20 18:15:33 +0800

    Point to correct bamboo build plan
    WT-7315

--- bin/cascade.sh
+++ bin/cascade.sh
@@ -3,7 +3,7 @@ set -o errexit ; set -o errtrace ; set -o pipefail
 
 curl --silent --show-error --fail -u ${BAMBOO_USER}:${BAMBOO_PASS} \
   -X POST -d "ARTIFACT1&ExecuteAllStages" \
-  https://bamboo.dev.wisetime.com/rest/api/latest/queue/CAPI-WAPA > result.xml
+  https://bamboo.dev.wisetime.com/rest/api/latest/queue/CAPI-WACRA > result.xml
 
 echo "Result: "
 cat result.xml
